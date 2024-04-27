28b50334dc29e7da1493f729cdf4f048e3806303
romgharti <romgharti@gmail.com>
2024-04-27 13:54:38 +0545

    Revert "Settings: Add support for parallel space"
    This reverts commit 5789e96b2221d641de0762baa6f25d594b791ea8.

--- src/com/android/settings/users/UserSettings.java
+++ src/com/android/settings/users/UserSettings.java
@@ -1531,7 +1531,7 @@ public class UserSettings extends SettingsPreferenceFragment
     int getRealUsersCount() {
         return (int) mUserManager.getUsers()
                 .stream()
-                .filter(user -> !user.isGuest() && !user.isProfile() && !user.isParallel())
+                .filter(user -> !user.isGuest() && !user.isProfile())
                 .count();
     }
 
